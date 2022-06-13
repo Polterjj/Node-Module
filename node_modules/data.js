721729c70b1d777485bdc92e3a6e04fc9b07d3c2
JJ <jjpdet@gmail.com>
2022-06-13 18:17:16 -0400

    Node Modules 1

---
+++ node_modules/data.js
@@ -0,0 +1,7 @@
+//numbers - an array of numbers (e.g. 1, 1, 2, 3, 5, 8)
+const numbers = [1, 1, 2, 3, 5, 8];
+const name = "Fibonacci";
+const hometown = "Hewlett";
+// name - a string (e.g. Fibonacci)
+// hometown - a string (e.g. Pisa, Italy)
+module.exports = { numbers, name, hometown };
